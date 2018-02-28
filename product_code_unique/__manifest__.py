b501504a0eb5623cd9efa8121c5f8165740353f5
Antonio Yamuta <agyamuta@users.noreply.github.com>
2018-02-27 23:40:42 -0800

    [ADD] Module to add a constraint on the internal reference of the product to make it unique across the database.

---
+++ product_code_unique/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Unique Product Internal Reference",
+    "summary": "Set Product Internal Reference as Unique",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Product",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["product"],
+    "data": [
+    ],
+    'demo': [
+        'data/product_demo.xml',
+    ],
+    "installable": True,
+}
