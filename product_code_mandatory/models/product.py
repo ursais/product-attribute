e42187011690afc715f1615fae458b48409cc869
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-03-01 21:45:02 -0600

    [IMP] Generate random default_code if null

--- product_code_mandatory/models/product.py
+++ product_code_mandatory/models/product.py
@@ -1,13 +1,17 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+import random
 from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    default_code = fields.Char('Internal Reference', index=True, required=True)
+    def _get_default_code(self):
+        return random.randint(100000000000,999999999999)
+
+    default_code = fields.Char('Internal Reference', index=True, required=True,
+                               default=_get_default_code)
 
 
 class ProductProduct(models.Model):
