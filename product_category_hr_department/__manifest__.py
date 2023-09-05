b5b4bc533ca1dea9dd3d064bf751268747fd4bc1
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2023-09-05 12:22:17 -0600

    [ADD] product_category_hr_department

---
+++ product_category_hr_department/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Product Category Hr Department Link",
+    "version": "16.0.1.0.1",
+    "category": "Product",
+    "summary": "Link product categories to hr departments",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": ["views/product_category_views.xml"],
+}
