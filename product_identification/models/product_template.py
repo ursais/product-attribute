4c7e218b42921654dcc1c0e0ab886592fced4d19
Chankya Soni <csoni@opensourceintegrators.com>
2025-02-24 13:39:18 -0500

    [FIX] Replaced product.product to product.template

--- product_identification/models/product_template.py
+++ product_identification/models/product_template.py
@@ -2,7 +2,7 @@ from odoo import fields, models
 
 
 class ProductProduct(models.Model):
-    _inherit = "product.product"
+    _inherit = "product.template"
 
     id_numbers_ids = fields.One2many(
         "product.identification", "product_id", string="Identification Numbers"
